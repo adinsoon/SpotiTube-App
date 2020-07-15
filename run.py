05d61e5e5c591823f53cf57c45b7b71f89e85534
Adrian S <67870240+adinsoon@users.noreply.github.com>
2020-07-15 17:28:51 +0200

    New functionality:  get_video_links (described in file) New functions related to API use

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from googleapiclient.discovery import build
 import json
 from secret import api_key
+import pprint
 
 # google api for python https://github.com/googleapis/google-api-python-client
 # api key taken from https://console.developers.google.com/
@@ -12,17 +13,69 @@ from secret import api_key
 youtube = build('youtube', 'v3', developerKey=api_key)
 
 
-def get_stats_from_channel(username='google'):
+def get_channel_id_from_username(username='google'):
+    request = youtube.channels().list(
+        part='statistics',
+        forUsername=username
+    )
+    response = request.execute()
+    response = response['items'][0]['id']
+    return response
+
+
+def get_stats_from_channel_username(username='google'):
     request = youtube.channels().list(
         part='statistics',
         forUsername=username
     )
     response = request.execute()
     response = response['items'][0]['statistics']
-    json_response = json.dumps(response, indent=4)
+    json_response = json.dumps(response, indent=2)
     return json_response
 
 
-print(get_stats_from_channel('twitter'))
+def get_playlists_from_channel_id(channel_id='UCK8sQmJBp8GCxrOtXWBpyEA'):
+    # show public playlists made by selected channel id
+    # due to lot of unnecessary data, function returns playlist's id and title in a list
+    request = youtube.playlists().list(
+        part='contentDetails, snippet',
+        channelId=channel_id,
+        maxResults=50
+    )
+    playlists = []
+    response = request.execute()
+    for playlist in response['items']:
+        playlist_id = playlist.get('id')
+        playlist_title = playlist.get('snippet')
+        playlists.append({'id': playlist_id, 'title': playlist_title['title']})
+    # for better visibility of returned elements, please write as following:
+    #
+    # for item in get_playlists_from_channel_id():
+    #   print("ID:", item.get('id'), "\t\tTITLE:", item.get('title'))
+    return playlists
 
 
+def get_content_of_playlist(playlist_id='PL590L5WQmH8eZnQfjoF91OImPfrmv-Vds'):
+    # show content of the given playlist
+    # note that only public videos from the public playlist will be shown
+    request = youtube.playlistItems().list(
+        part='snippet',
+        playlistId=playlist_id,
+        maxResults=50
+    )
+    videos = []
+    response = request.execute()
+    for video in response['items']:
+        video_id = str(video['snippet']['resourceId']['videoId'])
+        title = str(video['snippet']['title'])
+        description = str(video['snippet']['description']).replace("\n", " ")
+        videos.append({'id': video_id, 'title': title, 'desc': description})
+    # for better visibility of returned elements, please write as following:
+    #
+    # for videos in get_content_of_playlist():
+    #   print("ID:", videos.get('id'), "\tTITLE:", videos.get('title'), "\nDESC:", videos.get('desc'))
+    return videos
+
+
+for videos in get_content_of_playlist():
+    print("ID:", videos.get('id'), "\tTITLE:", videos.get('title'), "\nDESC:", videos.get('desc'), '\n', "-"*50)
